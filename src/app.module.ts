37005b4d0d18f349c9a545033be79f31320300bd
Aleksandr-Rabtsevich <aleksandr.rabtsevich@gcore.lu>
2021-02-20 08:48:41 +0300

    3

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.DB_URL), CatsModule],
+  imports: [
+    MongooseModule.forRoot(process.env.DB_URL || 'mongodb://mongo_db'),
+    CatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
